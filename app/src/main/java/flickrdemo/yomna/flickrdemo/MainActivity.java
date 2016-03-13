a3508a41a8475d7d87f3f666ed7e598cf041821c
YOmna <yomna.m.hafez@gmail.com>
2016-03-13 16:30:07 +0200

    using volley instead of ION

---
+++ app/src/main/java/flickrdemo/yomna/flickrdemo/MainActivity.java
@@ -0,0 +1,79 @@
+package flickrdemo.yomna.flickrdemo;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MainActivity extends Activity implements Response.Listener,
+        Response.ErrorListener {
+    public static final String REQUEST_TAG = "MainVolleyActivity";
+    private TextView mTextView;
+    private Button mButton;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        mButton = (Button) findViewById(R.id.button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mQueue = CustomVolleyRequestQueue.getInstance(this.getApplicationContext())
+                .getRequestQueue();
+        String url = "http://api.androidhive.info/volley/person_array.json";
+        final CustomJSONObjectRequest jsonRequest = new CustomJSONObjectRequest(Request.Method
+                .GET, url,
+                new JSONObject(), this, this);
+        jsonRequest.setTag(REQUEST_TAG);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mQueue.add(jsonRequest);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mQueue != null) {
+            mQueue.cancelAll(REQUEST_TAG);
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        mTextView.setText(error.getMessage());
+    }
+
+    @Override
+    public void onResponse(Object response) {
+        mTextView.setText("Response is: " + response);
+        try {
+            mTextView.setText(mTextView.getText() + "\n\n" + ((JSONObject) response).getString
+                    ("name"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
